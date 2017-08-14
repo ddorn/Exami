b89d4a81d39c47e5d669f7c69c80f91c60cc9853
ddorn <diego.dorn@free.fr>
2017-08-14 18:08:05 +1000

    Concistency extended (try + comments + doc)

--- Exami/Visual Layer/RoomDesigner.vb
+++ Exami/Visual Layer/RoomDesigner.vb
@@ -2,7 +2,8 @@
 
     Dim preview As RoomPreview
 
-    Private Sub Button1_Click() Handles CreateRoomButton.Click
+    Private Sub CreateRoomButton_Click() Handles CreateRoomButton.Click
+
         ' We hide the size stuff
         RowLabel.Visible = False
         ColumnLabel.Visible = False
@@ -33,18 +34,20 @@
     End Sub
 
     Private Sub SaveButton_Click(sender As Object, e As EventArgs) Handles SaveButton.Click
-        Dim result = InputBox("Enter the name of the room.")
+        Dim result As String
 
-        If result = "" Then
-            MsgBox("Invalid name, try something longer !")
-            Return
-        ElseIf result.Contains(".") Or result.Contains("/") Or result.Contains("\") Then
-            MsgBox("Do not use special characters.")
-            Return
-        End If
+        ' We ask until you answer niark
+        Dim prompt = "What is the name of this room ?"
+        While Not File.IsValidFileName(result)
+            result = InputBox(prompt)
+            prompt = "It is an invalid file name, try an other name for this room"
+        End While
 
         ' Save the room
-        DataAccessLayer.DD.SaveRoom(preview, IO.Path.Combine(ExamiForm.WorkingFolder, result + ".dd"))
+        If Not DataAccessLayer.DD.TrySaveRoom(preview, IO.Path.Combine(ExamiForm.WorkingFolder, result + ".dd")) Then
+            MsgBox("Error while saving the room. The name could be invalid, or you don have write access here...")
+            Return
+        End If
 
         ' Go back tothe main form
         Me.Close()
@@ -53,19 +56,33 @@
 
 End Class
 
-
+''' <summary>
+''' A room that displays its tables on the screen. Uses <c>RoomTable</c> to display one place.
+''' </summary>
 Class RoomPreview
     Inherits Room
 
     Dim tablesButtons As RoomTable(,)
     Public selectedTable As RoomTable
 
+    ''' <summary>
+    ''' Create a new Room with <paramref name="rowNb"/> rows and <paramref name="colNb"/> columns
+    ''' </summary>
+    ''' <param name="rowNb">The number of rows</param>
+    ''' <param name="colNb">The nmber of columns</param>
     Sub New(rowNb, colNb)
         MyBase.New(rowNb, colNb)
         ReDim tablesButtons(rowNb, colNb)
         SetRectangle(True, 0, 0, rowNb - 1, colNb - 1)
     End Sub
 
+    ''' <summary>
+    ''' Set the availability of a place, actualing the buttons and the arrays.
+    ''' </summary>
+    ''' <param name="row">The row of the table. Starts at zero</param>
+    ''' <param name="col">The column of the table, stars at zero</param>
+    ''' <param name="value">The availability of the place</param>
+    ''' <exception cref="IndexOutOfRangeException">If row or col is out of the bounds of the array / room</exception>
     Protected Overrides Sub SetAvailable(row As Byte, col As Byte, value As Boolean)
         MyBase.SetAvailable(row, col, value)
         If tablesButtons(row, col) Is Nothing Then
@@ -77,6 +94,9 @@ Class RoomPreview
 End Class
 
 
+''' <summary>
+''' One table for the RoomPreview. Shows the availability of a place with pride !
+''' </summary>
 Class RoomTable
     Inherits Button
 
@@ -84,6 +104,13 @@ Class RoomTable
     Public row, col As Byte
     Public roomPreviewParent As RoomPreview
 
+    ''' <summary>
+    ''' Initialise a new RoomTable with a given availability.
+    ''' </summary>
+    ''' <param name="row">The row of the table (starts at 0)</param>
+    ''' <param name="col">The col of the table (starts at 0)</param>
+    ''' <param name="roomPreviewParent">The RoomPreview object of which this table belongs</param>
+    ''' <param name="available">The availability of the table</param>
     Sub New(row As Byte, col As Byte, roomPreviewParent As RoomPreview, available As Boolean)
         MyBase.New()
         With Me
@@ -99,36 +126,57 @@ Class RoomTable
         Size = New Size(81, 50)  ' goldratio4ever
         Location = New Point(12 + col * (Width + 6), 12 + row * (Height + 6))
 
-        ' We add meto the form and allow events to be triggered or whatever the word is
+        ' We add me to the form and allow events to be triggered or whatever the word is
         RoomDesigner.Controls.Add(Me)
         AddHandler Me.Click, AddressOf ButtonClicked
 
     End Sub
 
+    ''' <summary>
+    ''' Performs the click in the button.
+    ''' Modify availability in the roomPreviewParent.
+    ''' </summary>
     Private Sub ButtonClicked()
+        ' If there is no previously selected button
         If roomPreviewParent.selectedTable Is Nothing Then
+            ' It is this one !
             roomPreviewParent.selectedTable = Me
+            ' Show it with some purple
             ForeColor = Color.MediumPurple
         Else
+            ' If there is already one
             Dim row2 = roomPreviewParent.selectedTable.row
             Dim col2 = roomPreviewParent.selectedTable.col
 
+            ' Change the rectangle of the preview 
             roomPreviewParent.ChangeAvailaible(row, col, row2, col2)
+            ' And reset the previously selected button 
             roomPreviewParent.selectedTable = Nothing
         End If
     End Sub
 
+    ''' <summary>
+    ''' Get or set the availability of this table.
+    ''' Sets the colors too ! :D Coloooooors
+    ''' </summary>
+    ''' <returns>The availability of the table</returns>
     Public Property Available() As Boolean
         Get
             Return _available
         End Get
+
         Set(ByVal value As Boolean)
+            ' If it is purple aka. selected we reset it
             ForeColor = Color.Black
+
+            ' Such beautiful colors, NEVER CHANGE THE ORANGE
             If value Then
                 BackColor = Color.GreenYellow
             Else
                 BackColor = Color.OrangeRed
             End If
+
+            ' Yeah, of course
             _available = value
         End Set
     End Property
