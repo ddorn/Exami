54624fec127a59596fffc4fc339f6baf35c787da
ddorn <diego.dorn@free.fr>
2017-08-31 18:08:28 +1000

    placementBox begin

---
+++ Exami/Visual Layer/PlacementBox.vb
@@ -0,0 +1,21 @@
+﻿Public Class PlacementBox
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
