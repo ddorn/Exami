afe40549fd4963f913328b6f35ddeace2a78f8c5
ddorn <diego.dorn@free.fr>
2017-09-03 14:18:54 +1000

    viewby has a value

--- Exami/Visual Layer/PlacementViewBySelector.vb
+++ Exami/Visual Layer/PlacementViewBySelector.vb
@@ -1,4 +1,16 @@
 ﻿Public Class PlacementViewBySelector
+
+    <Flags()>
+    Public Enum ViewBy
+        None = 0
+        Classe = 1
+        Room = 2
+        Subject = 4
+        All = ViewBy.Classe Or ViewBy.Room Or ViewBy.Subject
+    End Enum
+
+    Public CurrentViewBy As ViewBy = ViewBy.None
+
     Private Sub AllCheckBox_CheckedChanged() Handles AllCheckBox.Click
         ' We just set erything to the same value
 
@@ -15,6 +27,17 @@
         End If
     End Sub
 
+    ' Change of state
 
+    Private Sub SubjectCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles SubjectCheckBox.CheckedChanged
+        CurrentViewBy = CurrentViewBy Xor ViewBy.Subject
+    End Sub
 
+    Private Sub ClassCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles ClassCheckBox.CheckedChanged
+        CurrentViewBy = CurrentViewBy Xor ViewBy.Classe
+    End Sub
+
+    Private Sub RoomCheckBox_CheckedChanged(sender As Object, e As EventArgs) Handles RoomCheckBox.CheckedChanged
+        CurrentViewBy = CurrentViewBy Xor ViewBy.Room
+    End Sub
 End Class
