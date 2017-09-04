b06f03046e0c7769cbfe42a7f59b658edc9fcac9
ddorn <diego.dorn@free.fr>
2017-09-04 22:01:26 +1000

    comments ++

--- Exami/Visual Layer/Exami2.vb
+++ Exami/Visual Layer/Exami2.vb
@@ -121,38 +121,53 @@
         Dim places = DataAccessLayer.DD.LoadAllPlaces1D(RoomManager1.GetSelectedRoomPaths)
         Dim students = New StudentGroup(SubjectManager1.GetSelectedSubjectPaths)
 
+        ' We really want to have enough places
         If students.Count > places.Count Then
             RaiseEvent NewStatusMessage("There is more students than places.")
+            MsgBox("There is more students than places !", MsgBoxStyle.Exclamation)
             Return
         End If
 
         ' This is just removing the Room flag
         Dim groupBy = PlacementViewBySelector1.CurrentViewBy Or GroupType.Room Xor GroupType.Room
 
-        Dim groups = students.Separate(groupBy)
+        ' The idea is to do a virtual placement 
+        ' So sudents will have a room
+        ' And then we can separate them by room
 
-        Dim placed = New List(Of Student)
+        Dim groups = students.Separate(groupBy)
 
         Dim pos = 0
         For Each group In groups
             For studPos = 0 To group.Count - 1
                 group.allStudents(studPos).place = places(pos)
-                placed.Add(group.allStudents(studPos))
                 pos += 1
             Next
         Next
 
+        ' So we separate the sudents and alocating the places by chunck like u=in the previous for
+        ' it is likely that sudents wont have this place at the end, but the goal are 
+        '   1. to be able to already separate the clas
+        '   2. to have the same group in the same 'chunk' of places
+        '   3. I'm soory for not beiing clear
+
+        ' Thos dicts have the category name as key and a chunk of students or corresponding places as values
         Dim studDict = New Dictionary(Of String, StudentGroup)
         Dim placeDict = New Dictionary(Of String, List(Of Place))
 
-        groups = New StudentGroup(placed).Separate(PlacementViewBySelector1.CurrentViewBy)
+        ' The real groups with the rooms
+        groups = New StudentGroup(students.allStudents).Separate(PlacementViewBySelector1.CurrentViewBy)
         For Each group In groups
             Dim name = group.GetNameAs(PlacementViewBySelector1.CurrentViewBy)
             studDict(name) = group
+            ' We take the same number of places than of students
+            ' We take them in the list order
             placeDict(name) = places.Take(group.Count).ToList
+            ' And then remove them, they are used.
             places.RemoveRange(0, group.Count)
         Next
 
+        ' Finally we can send those two dicts to the placementBoxes wo will add one PlacementBox for each category
         PlacementBoxes1.SetPlacements(studDict, placeDict)
 
     End Sub
@@ -171,11 +186,13 @@
     ''' Controls are added recursively via SetUpHoverControl in Exami_Load.
     ''' </summary>
     ''' <seealso cref="SetUpHoverHandler(Control)"/>
-    Private Sub SetToolTipHelp(sender As Object, e As EventArgs)
-        Dim control = CType(sender, Control)
-        If control.Tag <> "" Then
-            HoverStatusLabel.Text = control.Tag
+    Private Sub SetToolTipHelp(sender As Control, e As EventArgs)
+
+        ' Only if this control has a tag, we show it in the help status
+        If sender.Tag <> "" Then
+            HoverStatusLabel.Text = sender.Tag
         End If
+
     End Sub
 
     ''' <summary>
@@ -184,23 +201,26 @@
     ''' <param name="control">The control to add to the ToolTipHelp</param>
     ''' <seealso cref="SetToolTipHelp(Object, EventArgs)"/>
     Sub SetUpHoverHandler(control As Control)
+
+        ' We add the handler
         AddHandler control.MouseEnter, AddressOf SetToolTipHelp
         AddHandler control.Enter, AddressOf SetToolTipHelp
 
+        ' And the same for every sub control
         For Each con In control.Controls
             SetUpHoverHandler(con)
         Next
+
     End Sub
 
     ''' <summary>
     ''' Set the message in the botton right cornr of the app.
     ''' this corner is inteded tobe used for general purpose messages, warnings and jokes ^^
     ''' </summary>
-    ''' <param name="msg"></param>
+    ''' <param name="msg">The new text of the generak message status</param>
     Sub SetStatusMessage(msg As String) Handles Me.NewStatusMessage, RoomManager1.NewStatusMessage
         GeneralStatusLabel.Text = msg
     End Sub
-
 
 End Class
 
