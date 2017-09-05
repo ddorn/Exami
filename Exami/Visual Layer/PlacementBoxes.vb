23da818ee3a3835f4e3838cdca5db2ce30049904
ddorn <diego.dorn@free.fr>
2017-09-05 10:59:07 +1000

    Comments 4

--- Exami/Visual Layer/PlacementBoxes.vb
+++ Exami/Visual Layer/PlacementBoxes.vb
@@ -1,12 +1,26 @@
-﻿Public Class PlacementBoxes
+﻿''' <summary>
+''' This control is just a list of 
+''' </summary>
+Public Class PlacementBoxes
 
-    Private PlacementBoxList As List(Of PlacementBox) = New List(Of PlacementBox)
+    Public Event NewMessage(nsg As String)
 
+    ''' <summary>
+    ''' Creates a PlacementBox for each keys of the two dictionnaries.
+    ''' </summary>
+    ''' <param name="students">A dictionnary of the names of the Boxes as key and a student group as key. The Keys must be the same as the <paramref name="places"/>...
+    ''' and the number of students by student group se same as the corresponding list of places.</param>
+    ''' <param name="places">A dictionnary with the exact same structure as <paramref name="students"/>.</param>
     Public Sub SetPlacements(students As Dictionary(Of String, StudentGroup), places As Dictionary(Of String, List(Of Place)))
 
         Controls.Clear()
 
-        Dim boxWidth = Me.Width / students.Count - 3
+        If students.Count <> places.Count Or students.Count = 0 Then
+            RaiseEvent NewMessage("Wow, there a problem... Check PlacementBoxes.SetPlacements...")
+            Return
+        End If
+
+        Dim boxWidth = Math.Max(Me.Width / students.Count - 3, 300)
         Dim curPosX = 0
         Dim box As PlacementBox = Nothing
 
@@ -18,7 +32,6 @@
                 .Size = New Size(boxWidth, Me.Height)
                 .Title = boxName
                 .Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Bottom
-
             End With
 
             Me.Controls.Add(box)
