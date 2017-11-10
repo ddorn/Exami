565befaa70f3871a8fe78d166d8102f8f6ec8815
ddorn <diego.dorn@free.fr>
2017-11-10 12:42:05 +1100

    resized Option selector

--- Exami/Visual Layer/Exami2.Designer.vb
+++ Exami/Visual Layer/Exami2.Designer.vb
@@ -25,9 +25,6 @@ Partial Class Exami2
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Exami2))
         Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
         Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
-        Me.SubjectManager1 = New Exami.SubjectManager()
-        Me.RoomManager1 = New Exami.RoomManager()
-        Me.PlacementBoxes1 = New Exami.PlacementBoxes()
         Me.SelectFolderButton = New System.Windows.Forms.Button()
         Me.MakePlacementButton = New System.Windows.Forms.Button()
         Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
@@ -43,6 +40,9 @@ Partial Class Exami2
         Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
         Me.SettingsButton = New System.Windows.Forms.Button()
         Me.OptionsSelector1 = New Exami.OptionsSelector()
+        Me.SubjectManager1 = New Exami.SubjectManager()
+        Me.RoomManager1 = New Exami.RoomManager()
+        Me.PlacementBoxes1 = New Exami.PlacementBoxes()
         CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SplitContainer1.Panel1.SuspendLayout()
         Me.SplitContainer1.Panel2.SuspendLayout()
@@ -101,37 +101,6 @@ Partial Class Exami2
         Me.SplitContainer2.SplitterDistance = 210
         Me.SplitContainer2.TabIndex = 21
         Me.SplitContainer2.TabStop = False
-        '
-        'SubjectManager1
-        '
-        Me.SubjectManager1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.SubjectManager1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SubjectManager1.Location = New System.Drawing.Point(3, 3)
-        Me.SubjectManager1.Name = "SubjectManager1"
-        Me.SubjectManager1.Size = New System.Drawing.Size(246, 202)
-        Me.SubjectManager1.TabIndex = 0
-        '
-        'RoomManager1
-        '
-        Me.RoomManager1.AutoSize = True
-        Me.RoomManager1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.RoomManager1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.RoomManager1.Location = New System.Drawing.Point(0, 0)
-        Me.RoomManager1.MinimumSize = New System.Drawing.Size(254, 0)
-        Me.RoomManager1.Name = "RoomManager1"
-        Me.RoomManager1.Padding = New System.Windows.Forms.Padding(3)
-        Me.RoomManager1.Size = New System.Drawing.Size(254, 263)
-        Me.RoomManager1.TabIndex = 0
-        '
-        'PlacementBoxes1
-        '
-        Me.PlacementBoxes1.AutoScroll = True
-        Me.PlacementBoxes1.AutoSize = True
-        Me.PlacementBoxes1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.PlacementBoxes1.Location = New System.Drawing.Point(0, 0)
-        Me.PlacementBoxes1.Name = "PlacementBoxes1"
-        Me.PlacementBoxes1.Size = New System.Drawing.Size(0, 0)
-        Me.PlacementBoxes1.TabIndex = 0
         '
         'SelectFolderButton
         '
@@ -252,12 +221,45 @@ Partial Class Exami2
         'OptionsSelector1
         '
         Me.OptionsSelector1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.OptionsSelector1.AutoSize = True
+        Me.OptionsSelector1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.OptionsSelector1.Enabled = False
-        Me.OptionsSelector1.Location = New System.Drawing.Point(680, 8)
+        Me.OptionsSelector1.Location = New System.Drawing.Point(619, 8)
         Me.OptionsSelector1.MinimumSize = New System.Drawing.Size(237, 43)
         Me.OptionsSelector1.Name = "OptionsSelector1"
-        Me.OptionsSelector1.Size = New System.Drawing.Size(490, 43)
+        Me.OptionsSelector1.Size = New System.Drawing.Size(556, 48)
         Me.OptionsSelector1.TabIndex = 8
+        '
+        'SubjectManager1
+        '
+        Me.SubjectManager1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.SubjectManager1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SubjectManager1.Location = New System.Drawing.Point(3, 3)
+        Me.SubjectManager1.Name = "SubjectManager1"
+        Me.SubjectManager1.Size = New System.Drawing.Size(246, 202)
+        Me.SubjectManager1.TabIndex = 0
+        '
+        'RoomManager1
+        '
+        Me.RoomManager1.AutoSize = True
+        Me.RoomManager1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.RoomManager1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RoomManager1.Location = New System.Drawing.Point(0, 0)
+        Me.RoomManager1.MinimumSize = New System.Drawing.Size(254, 0)
+        Me.RoomManager1.Name = "RoomManager1"
+        Me.RoomManager1.Padding = New System.Windows.Forms.Padding(3)
+        Me.RoomManager1.Size = New System.Drawing.Size(254, 263)
+        Me.RoomManager1.TabIndex = 0
+        '
+        'PlacementBoxes1
+        '
+        Me.PlacementBoxes1.AutoScroll = True
+        Me.PlacementBoxes1.AutoSize = True
+        Me.PlacementBoxes1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.PlacementBoxes1.Location = New System.Drawing.Point(0, 0)
+        Me.PlacementBoxes1.Name = "PlacementBoxes1"
+        Me.PlacementBoxes1.Size = New System.Drawing.Size(0, 0)
+        Me.PlacementBoxes1.TabIndex = 0
         '
         'Exami2
         '
