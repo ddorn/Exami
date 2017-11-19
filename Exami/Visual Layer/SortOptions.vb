7a9c8c291951d2e17c1bfbfe39c7224878e45540
ddorn <diego.dorn@free.fr>
2017-11-19 15:47:00 +1100

    switch image on snake button

--- Exami/Visual Layer/SortOptions.vb
+++ Exami/Visual Layer/SortOptions.vb
@@ -3,6 +3,7 @@
     Public Event SortChanged()
 
     Public GroupClasses As Boolean = True
+    Public Snake As Boolean = True
 
     Private _sortby As SortBy = SortBy.Name
     Public Property Sort() As SortBy
@@ -23,11 +24,11 @@
         Me.Sort = SortBy.Number
     End Sub
 
-    Private Sub AzSortButton_Click(sender As Object, e As EventArgs) Handles AzSortButton.Click
+    Private Sub AzSortButton_Click() Handles AzSortButton.Click
         Me.Sort = SortBy.Name
     End Sub
 
-    Private Sub GroupClassesButton_Click(sender As Object, e As EventArgs) Handles GroupClassesButton.Click
+    Private Sub GroupClassesButton_Click() Handles GroupClassesButton.Click
         Me.GroupClasses = Not Me.GroupClasses
         If GroupClasses Then
             GroupClassesButton.BackgroundImage = My.Resources.group
@@ -37,6 +38,17 @@
             GroupClassesButton.Tag = "Group Students by classes"
         End If
         RaiseEvent SortChanged()
+    End Sub
+
+    Private Sub SnakeButton_Click() Handles SnakeButton.Click
+        Me.Snake = Not Me.Snake
+        If Snake Then
+            SnakeButton.BackgroundImage = My.Resources.snake2
+            SnakeButton.Tag = "Click to place the students front to back in every column"
+        Else
+            SnakeButton.BackgroundImage = My.Resources.no_snake2
+            SnakeButton.Tag = "Click to place the students in a snake patern: front to back the back to front"
+        End If
     End Sub
 
 End Class
